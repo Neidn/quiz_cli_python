21e67eb4ea03756d58fbce39ae1c9e19f9897a54
Neidn <dty3152@gmail.com>
2024-01-30 13:01:16 +0900

    still has question

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ from quiz_brain import QuizBrain
 question_bank = [Question(question["text"], question["answer"]) for question in question_data]
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()

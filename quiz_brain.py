2e44868a35263a23968e733a3c0f72c3b5a0a287
Neidn <dty3152@gmail.com>
2024-01-30 13:08:28 +0900

    check answer and show scores

--- quiz_brain.py
+++ quiz_brain.py
@@ -12,6 +12,21 @@ class QuizBrain:
         current_question = self.question_list[self.question_number]
         self.question_number += 1
         user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
+        self.check_answer(user_answer, current_question.answer)
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
+
+    def check_answer(self, user_answer, correct_answer):
+        user_answer = user_answer.lower()
+        correct_answer = correct_answer.lower()
+
+        if user_answer == correct_answer:
+            self.score += 1
+            print("You got it right!")
+        else:
+            print("That's wrong.")
+
+        print(f"The correct answer was: {correct_answer}.")
+        print(f"Your current score is: {self.score}/{self.question_number}.\n")
+

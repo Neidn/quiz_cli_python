e2e430601d4941b85295eabec137e5ac90f7063c
Neidn <dty3152@gmail.com>
2024-01-30 13:18:39 +0900

    connect data to Open Trivia DB

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from question_model import Question
-from data import question_data
 from quiz_brain import QuizBrain
+from data import question_data
 
-question_bank = [Question(question["text"], question["answer"]) for question in question_data]
+question_bank = [Question(question["question"], question["correct_answer"]) for question in question_data]
 
 quiz = QuizBrain(question_bank)
 
